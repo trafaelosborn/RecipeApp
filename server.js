d810f78c9f4710a827904434825d8d1ec90a0c12
Taylor Osborn <osborn.taylor@gmail.com>
2020-04-13 13:57:09 -0700

    ...

--- server.js
+++ server.js
@@ -20,10 +20,16 @@ app.use(express.json());
 	})
 	.then(() => console.log("mongodb connected"))
 	.catch(err => console.log(err)); */
-if (process.env.NODE_ENV === "production") {
-	// Set static folder
-	app.use(express.static("client/build"));
-}
+	if (process.env.NODE_ENV === 'production') {
+		// Exprees will serve up production assets
+		app.use(express.static('client/build'));
+	  
+		// Express serve up index.html file if it doesn't recognize route
+		const path = require('path');
+		app.get('*', (req, res) => {
+		  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		});
+	  }
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/recipedb");
 
 // use routes
